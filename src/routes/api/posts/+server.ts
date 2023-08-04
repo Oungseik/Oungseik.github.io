b93f57ceaf4077a05b81be38fd556866532723b3
Oungseik <mhemaungthuwin@gmail.com>
2023-08-04 09:30:30 +0630

    feat: organize posts with its assets

--- src/routes/api/posts/+server.ts
+++ src/routes/api/posts/+server.ts
@@ -8,37 +8,37 @@ import fm from "front-matter";
 const INTERVAL = 1000 * 60 * 60 * 24 * 7; // a week
 
 const postsDB: PostsDB = {
-	posts: [],
-	lastUpdate: new Date(),
+  posts: [],
+  lastUpdate: new Date(),
 };
 
 async function readFile(path: string) {
-	const data = await fs.readFile(path, { encoding: "utf-8" });
-	return fm(data);
+  const data = await fs.readFile(path, { encoding: "utf-8" });
+  return fm(data);
 }
 
 async function setupDB() {
-	const cwd = process.cwd();
-	const paths = (await fs.readdir(`${cwd}/src/posts`)).map((file) => `${cwd}/src/posts/${file}`);
-	const data = (await Promise.allSettled(paths.map(readFile))) as {
-		status: string;
-		value: { attributes: Post; body: string };
-	}[];
-	postsDB.posts = data.map((d) => d.value.attributes);
+  const cwd = process.cwd();
+  const paths = (await fs.readdir(`${cwd}/static/posts`)).map((path) => `${cwd}/static/posts/${path}/index.md`);
+  const data = (await Promise.allSettled(paths.map(readFile))) as {
+    status: string;
+    value: { attributes: Post; body: string };
+  }[];
+  postsDB.posts = data.map((d) => d.value.attributes);
 
-	return function getPosts() {
-		return postsDB.posts
-			.filter((post) => post.published)
-			.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
-	};
+  return function getPosts() {
+    return postsDB.posts
+      .filter((post) => post.published)
+      .sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
+  };
 }
 
 const getPosts = await setupDB();
 
 export async function GET() {
-	if (new Date().getTime() - new Date(postsDB.lastUpdate).getTime() > INTERVAL) {
-		const posts = (await setupDB())();
-		return json(posts);
-	}
-	return json(getPosts());
+  if (new Date().getTime() - new Date(postsDB.lastUpdate).getTime() > INTERVAL) {
+    const posts = (await setupDB())();
+    return json(posts);
+  }
+  return json(getPosts());
 }
