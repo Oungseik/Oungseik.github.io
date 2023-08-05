9dbd5fa78684febe4c2fe040d17e590cdae50789
Oungseik <mhemaungthuwin@gmail.com>
2023-08-05 17:12:13 +0630

    feat: add some styling

--- src/routes/blogs/[slug]/+page.svelte
+++ src/routes/blogs/[slug]/+page.svelte
@@ -42,13 +42,41 @@
 		style="background-image: url(/images/posts/{data.meta.mainImage});"
 	/>
 
-	<h1 class="mt-2">{data.meta.title}</h1>
-	<ul class="mb-4 flex gap-4 text-slate-700 md:mb-8">
-		<li>{formatDate(data.meta.date)}</li>
-		{#if data.meta.author}
-			<li>{data.meta.author}</li>
-		{/if}
-		<li>{data.meta.estimateRead}</li>
+	<h1 class="mt-8">{data.meta.title}</h1>
+	<ul class="mb-6 mt-2 flex gap-4 text-slate-700 md:mb-10">
+		<!-- {#if data.meta.author} -->
+		<!-- 	<li class="flex gap-1"> -->
+		<!-- 		<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" -->
+		<!-- 			><path -->
+		<!-- 				fill="currentColor" -->
+		<!-- 				d="M12 12q-1.65 0-2.825-1.175T8 8q0-1.65 1.175-2.825T12 4q1.65 0 2.825 1.175T16 8q0 1.65-1.175 2.825T12 12Zm-8 8v-2.8q0-.85.438-1.563T5.6 14.55q1.55-.775 3.15-1.163T12 13q1.65 0 3.25.388t3.15 1.162q.725.375 1.163 1.088T20 17.2V20H4Zm2-2h12v-.8q0-.275-.138-.5t-.362-.35q-1.35-.675-2.725-1.012T12 15q-1.4 0-2.775.338T6.5 16.35q-.225.125-.363.35T6 17.2v.8Zm6-8q.825 0 1.413-.588T14 8q0-.825-.588-1.413T12 6q-.825 0-1.413.588T10 8q0 .825.588 1.413T12 10Zm0-2Zm0 10Z" -->
+		<!-- 			/></svg -->
+		<!-- 		> -->
+		<!-- 		{data.meta.author} -->
+		<!-- 	</li> -->
+		<!-- {/if} -->
+		<li class="flex gap-1">
+			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+				><path
+					fill="none"
+					stroke="currentColor"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="1.5"
+					d="M15 4V2m0 2v2m0-2h-4.5M3 10v9a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-9H3Zm0 0V6a2 2 0 0 1 2-2h2m0-2v4m14 4V6a2 2 0 0 0-2-2h-.5"
+				/></svg
+			>
+			{formatDate(data.meta.date)}
+		</li>
+		<li class="hidden gap-1 sm:flex">
+			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+				><path
+					fill="currentColor"
+					d="M12 20a8 8 0 0 0 8-8a8 8 0 0 0-8-8a8 8 0 0 0-8 8a8 8 0 0 0 8 8m0-18a10 10 0 0 1 10 10a10 10 0 0 1-10 10C6.47 22 2 17.5 2 12A10 10 0 0 1 12 2m.5 5v5.25l4.5 2.67l-.75 1.23L11 13V7h1.5Z"
+				/></svg
+			>
+			{data.meta.estimateRead}
+		</li>
 	</ul>
 	{@html data.content}
 </main>
--- static/posts/how-to-send-email-with-nodemailer-and-gmail.md
+++ static/posts/how-to-send-email-with-nodemailer-and-gmail.md
@@ -4,16 +4,29 @@ description: In this post I will show you how to setup nodemailer and gmail to s
 keywords: ["nodemailer", "email", "send email", "nodemailer and email", "send email with nodejs"]
 date: "2023-8-4"
 mainImage: "send-email.jpg"
+author: "Oung Seik Nyan"
 estimateRead: 15mins read
 categories:
   - Node.js
 published: true
 ---
 
-Hey friends! 👋
-
 ```ts
 function greet(name: string) {
 	console.log(`Hey ${name}! 👋`);
 }
+```
+
+## Setup the Development Environment
+
+Nodemailer is a Node.js module that prides itself as the solution most Node developers turn to by default for sending emails with Node.js and for good reason. It's pretty easy to set up nodemailer to start sending emails due to its features.
+
+Most times, you would need a pretty quick way to start sending emails in your Node application when you're building for development and that's where Gmail comes in.
+
+```bash
+#npm
+npm install nodemailer
+
+#yarn
+yarn add nodemailer
 ```
