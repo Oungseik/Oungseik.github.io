7ef425b58fd7bf25f326e153bb4f919cc0a94004
Oungseik <mhemaungthuwin@gmail.com>
2023-08-15 13:16:33 +0630

    reduce white space

--- src/routes/SkillsSection.svelte
+++ src/routes/SkillsSection.svelte
@@ -112,7 +112,7 @@
 	];
 </script>
 
-<section class="overflow-hidden px-4 py-14 pt-0 md:px-10 md:py-20">
+<section class="overflow-hidden px-4 py-14 pt-0 md:px-10 md:py-20 md:pt-0">
 	<div class="mx-auto max-w-3xl">
 		<div class="mb-6 text-center">
 			<h2 class="text-2xl font-medium md:text-4xl" id="skills">Skills</h2>
